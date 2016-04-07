32f2ba2b356122839c623c92ffb503eb3952c6a3
pwn777 <samroy92@live.com>
2016-04-06 23:14:21 -0700

    Update ByteBistroGame.py

--- ByteBistroGame.py
+++ ByteBistroGame.py
@@ -1,33 +1,40 @@
 # CST 205 - Lab 7
 # Team: Byte Bistro
 # April 4, 2016
+
+# Global variables for player items in game
 hasGoldenKey = false
 hasHairPin = false
 hasRope = false
 doorIsOpen = false
 windowIsOpen = false
-gameRun = true
+gameRun = true # main condition for game loop
 
 def playGame():
-  displayHelp()
-  currentRoom = getNextRoom("Den")
-  prompt(currentRoom)
-  while gameRun:
+
+  displayHelp() # displays help prompt with available commands
+  currentRoom = getNextRoom("Den") # set currentRoom to "Den" to start the game
+  prompt(currentRoom) # displays the current room and description to the user
+  
+  while(gameRun):
     input = raw_input("What would you like to do? ")
+    
+    # begining of main if-else statements. Each case is a user input of type: string
     if input == "help":
       displayHelp()
-      input = raw_input("Press any key to continue. ")
-      prompt(currentRoom)
+      input = raw_input("Press ENTER to continue. ") # pauses the screen at the help dialouge
+      prompt(currentRoom) # re-print the current room information
       continue
-    elif input == "north" or input == "east" or input == "south" or input == "west":
-      currentRoom = goDirection(currentRoom, input)
-      prompt(currentRoom)
+    # main choices for directions of movement
+    elif input == "north" or input == "east" or input == "south" or input == "west": 
+      currentRoom = goDirection(currentRoom, input) # set the currentRoom to the new room using the direction
+      prompt(currentRoom) # display the new room information
     elif input == "exit":
-      break;
+      break # exit kills the main loop condition
     elif input == "look":
-      prompt(currentRoom)
+      prompt(currentRoom) # 
     elif input == "open":
-      if doesCommandExist(currentRoom, input):
+      if doesCommandExist(currentRoom):
         object = raw_input("What would you like to open? ")
         string = ""
         for i in range(0, len(currentRoom[4])):
@@ -62,46 +69,109 @@ def getNextRoom(string):
     return getDeck()
     
 def getLounge():
-  return ["Lounge", ["north", "south", "west"], ["Library", "Kitchen", "Study"], "You've entered the Lounge.  A portrait of a man hangs in the center.  It feels like someone is watching you.", ["portrait"], ["\nThere is nothing behind this portrait, your imagination is playing with your head"]]
+  return ["Lounge", 
+         ["north", "south", "west"], 
+         ["Library", "Kitchen", "Study"], 
+         "You've entered the Lounge.  A portrait of a man hangs in the center.  It feels like someone is watching you.", 
+         ["portrait"], 
+         ["\nThere is nothing behind this portrait, your imagination is playing with your head"]]
 
 def getDen():    
-    return ["Den", ["north", "east"], ["Study", "Kitchen"], "You've entered the Den. There is a large oriental rug in the center of the room and a coffee table. There is an unopened letter sitting on the table.", ["letter"], ["\n(picks up letter)\nGreetings, You must escape this house. There is only one way out! HAHAHA!"]]
+  return ["Den", 
+         ["north", "east"],
+         ["Study", "Kitchen"], 
+         "You've entered the Den. There is a large oriental rug in the center of the room and a coffee table. There is an unopened letter sitting on the table.", 
+         ["letter"], 
+         ["\n(picks up letter)\nGreetings, You must escape this house. There is only one way out! HAHAHA!"]]
 
 def getKitchen():
-    return ["Kitchen", ["north", "west"], ["Lounge", "Den"], "You've entered the kitchen. There is a cabinet and a drawer.", ["cabinet", "drawer"], ["plate", "golden key"]]
+  return ["Kitchen", 
+         ["north", "west"], 
+         ["Lounge", "Den"], 
+         "You've entered the kitchen. There is a cabinet and a drawer.", 
+         ["cabinet", "drawer"], 
+         ["plate", "golden key"]]
 
 def getStudy():
-    return ["Study", ["south", "east"], ["Den", "Lounge"], "You've entered the study.  There is a book on the table and a bottled beverage.  You take a seat and rest for a bit.", ["book","beverage"], ["hair pin", "You open the beverage and take a sip.  You feel refreshed!"]]
+  return ["Study", 
+         ["south", "east"], 
+         ["Den", "Lounge"], 
+         "You've entered the study.  There is a book on the table and a bottled beverage.  You take a seat and rest for a bit.", 
+         ["book","beverage"], 
+         ["hair pin", "You open the beverage and take a sip.  A sharp pain roars through your chest and you fall to the floor grasping for air.\nYou died.  Better luck next time!"]]
 
 def getBallroom():
-    return ["Ballroom", ["west"], ["Library"], "Music is playing faintly from a jukebox, but no one is around.  You get an eerie feeling down your spine.  ", [], []]
+  return ["Ballroom", 
+         ["west"], 
+         ["Library"], 
+         "Music is playing faintly from a jukebox, but no one is around.  You get an eerie feeling down your spine.  ", 
+         [], 
+         []]
 
 def getConservatory():
-    if windowIsOpen == true:
-      return ["Conservatory", ["south", "west"], ["Library", "Deck"], "You've entered the conservatory.  On the west side of the room you see the open window that leads to a deck.  A chest sits next to it.", ["window","chest"], ["The window is already open! A slight breeze enters the room.", "rope"]]  
-    elif hasHairPin == true:
-      return ["Conservatory", ["south"], ["Library"], "You've entered the conservatory.  On the west side of the room you see a window that leads to a deck.  A chest sits next to it.", ["window","chest"], ["You try to pick the lock with the hair pin. 'click' You've opened the window!", "rope"]]
-    else:
-      return ["Conservatory", ["south"], ["Library"], "You've entered the conservatory.  On the west side of the room you see a window that leads to a deck.  A chest sits next to it.", ["window","chest"], ["The window is locked. Only if you had something to pick the lock. Maybe you should get some rest.","rope"]]
+  if windowIsOpen == true:
+    return ["Conservatory", 
+           ["south", "west"], 
+           ["Library", "Deck"], 
+           "You've entered the conservatory.  On the west side of the room you see the open window that leads to a deck.  A chest sits next to it.", 
+           ["window","chest"], 
+           ["The window is already open! A slight breeze enters the room.", "rope"]]  
+  elif hasHairPin == true:
+    return ["Conservatory", 
+           ["south"], ["Library"], 
+           "You've entered the conservatory.  On the west side of the room you see a window that leads to a deck.  A chest sits next to it.", 
+           ["window","chest"], 
+           ["You try to pick the lock with the hair pin. 'click' You've opened the window! Run 'west' to get away! ", "rope"]]
+  else:
+    return ["Conservatory", 
+           ["south"], 
+           ["Library"], 
+           "You've entered the conservatory.  On the west side of the room you see a window that leads to a deck.  A chest sits next to it.", 
+           ["window","chest"], 
+           ["The window is locked. Only if you had something to pick the lock. Maybe you should get some rest.","rope"]]
 
 def getDeck():
-    if hasRope == true:
-      printNow("You use the rope to climb down from the deck.  You reached the ground safely.  You've escaped the mansion.")
-      global gameRun
-      gameRun = false   
-      return ["clear!", ["endless"], [""], "You won!", [], []]
-    else: 
-      return ["Deck", ["east"], ["Conservatory"], "You stand out on the deck. You would not survive the fall. If only you had some rope.", [], []]
+  if hasRope == true:
+    printNow("You use the rope to climb down from the deck.  You reached the ground safely.  You've escaped the mansion.")
+    global gameRun
+    gameRun = false   
+    return ["clear!", 
+           ["endless"], 
+           [""], 
+           "You won!", 
+           [], 
+           []]
+  else: 
+    return ["Deck", 
+           ["east"], 
+           ["Conservatory"], 
+           "You stand out on the deck. You would not survive the fall. If only you had some rope.", 
+           [], 
+           []]
 
 def getLibrary():
-    if doorIsOpen == true:
-      return ["Library", ["north", "south", "east"], ["Conservatory", "Lounge", "Ballroom"], "You are in the library and you stand before the open gold door", ["door"], ["... wait the door is already open!"]]
-    elif hasGoldenKey == true:
-      return ["Library", ["south", "east"], ["Lounge", "Ballroom"], "You step into the library. A large door is north of you. It appears to be solid gold", ["door"], ["You try your golden key on the door and it opens easily. You have opened the golden door!"]]
-    else:
-      return ["Library", ["south", "east"], ["Lounge", "Ballroom"], "You step into the library. A large door is north of you.  It appears to be solid gold", ["door"], ["It wont budge. You need a key to open this door."]]
+  if doorIsOpen == true:
+    return ["Library", 
+           ["north", "south", "east"], 
+           ["Conservatory", "Lounge", "Ballroom"], 
+           "You are in the library and you stand before the open gold door", 
+           ["door"], 
+           ["... wait the door is already open!"]]
+  elif hasGoldenKey == true:
+    return ["Library", 
+           ["south", "east"], 
+           ["Lounge", "Ballroom"], 
+           "You step into the library. A large door is north of you. It appears to be solid gold", 
+           ["door"], 
+           ["You try your golden key on the door and it opens easily. You have opened the golden door!"]]
+  else:
+    return ["Library", 
+           ["south", "east"], 
+           ["Lounge", "Ballroom"], 
+           "You step into the library. A large door is north of you.  It appears to be solid gold", 
+           ["door"], 
+           ["It wont budge. You need a key to open this door."]]
     
-
 def executeCommand(command, container, object):
   if command == "open":
     if object == "golden key":
@@ -128,13 +198,13 @@ def executeCommand(command, container, object):
     else:
       return "You have opened the " + container + " and found a(n) " + object
 
-def doesCommandExist(room, command):
+def doesCommandExist(room):
   return len(room[5])
-
+  
 def prompt(currentRoom):
-    printNow("\nYou are currently in the " + currentRoom[0])
-    printNow(currentRoom[3])
-    printPossibleDirections(currentRoom)
+  printNow("\nYou are currently in the " + currentRoom[0])
+  printNow(currentRoom[3])
+  printPossibleDirections(currentRoom)
 
 def goDirection(cur, dir):
   for i in range(0, len(cur[1])):
@@ -155,4 +225,12 @@ def printPossibleDirections(room):
             
 def displayHelp():
   printNow("You open your eyes and find yourself in an unfamilliar mansion.  You can't remember what happened to you.  You take a look around and wonder 'Where am I'.") 
-  printNow("\nAvailable commands:\n'north', 'south', 'east', 'west', 'open', 'help' \nexit - Quit the game")
+  printNow("\nAvailable commands: ")
+  printNow("north     -    move the player north")
+  printNow("south    -    move the player south")
+  printNow("east      -    move the player east")
+  printNow("west     -    move the player west")
+  printNow("look      -    prints the current room information")
+  printNow("open     -    prompts the user to open a container in the room")
+  printNow("help      -    prints the help dialouge")
+  printNow("exit       -    ends the current game")
